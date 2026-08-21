f3c015bca7ea0494d33c4a18982fb31cf2636e73
Abdelrahman Malek <amm2592000@gmail.com>
2026-08-21 12:16:36 +0000

    Save current AWS streaming pipeline before S3 partitioning

--- streaming_job.py
+++ streaming_job.py
@@ -35,11 +35,13 @@ OPENSEARCH_PASSWORD = os.getenv("OPENSEARCH_PASSWORD")
 # ===========================
 
 
+import os
+
 REDSHIFT_HOST = os.getenv("REDSHIFT_HOST")
-REDSHIFT_DATABASE = os.getenv("REDSHIFT_DATABASE")
+REDSHIFT_PORT = os.getenv("REDSHIFT_PORT", "5439")
+REDSHIFT_DB = os.getenv("REDSHIFT_DB", "dev")
 REDSHIFT_USER = os.getenv("REDSHIFT_USER")
 REDSHIFT_PASSWORD = os.getenv("REDSHIFT_PASSWORD")
-
 
 df = (
     spark.readStream
@@ -507,7 +509,7 @@ bronze_s3_query = bronze_df.writeStream \
 #             "validation_reason"
 #             ).write \
 #             .format("jdbc") \
-#             .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#             .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #             .option("dbtable", "machine_events_quarantine") \
 #             .option("user", "admin") \
 #             .option("password", REDSHIFT_PASSWORD) \
@@ -538,53 +540,52 @@ bronze_s3_query = bronze_df.writeStream \
 
 
 
-# def write_machine_bronze_to_redshift(batch_df, batch_id):
+def write_machine_bronze_to_redshift(batch_df, batch_id):
 
-#     print(f"[BRONZE] Batch {batch_id}")
+    print(f"[BRONZE] Batch {batch_id}")
 
-#     batch_df.select(
-#         "event_id",
-#         "timestamp",
-#         "machine_id",
-#         "machine_type",
-#         "floor",
-#         "shift",
-#         "status",
-#         "error_code",
-#         "is_fault",
+    batch_df.select(
+        "event_id",
+        "timestamp",
+        "machine_id",
+        "machine_type",
+        "floor",
+        "shift",
+        "status",
+        "error_code",
+        "is_fault",
 
-#         "temperature",
-#         "vibration",
-#         "rpm",
-#         "power_kw",
+        "temperature",
+        "vibration",
+        "rpm",
+        "power_kw",
 
-#         "cnc_oil",
-#         "coolant_pressure",
+        "cnc_oil",
+        "coolant_pressure",
 
-#         "joint_torque",
-#         "force",
+        "joint_torque",
+        "force",
 
-#         "belt_tension",
-#         "load_weight",
+        "belt_tension",
+        "load_weight",
 
-#         "flow_rate",
-#         "inlet_pressure"
-#     ).write \
-#         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
-#         .option("dbtable", "machine_events_bronze") \
-#         .option("user", "admin") \
-#         .option("password", REDSHIFT_PASSWORD) \
-#         .option("driver", "com.amazon.redshift.jdbc.Driver") \
-#         .mode("append") \
-#         .save()
+        "flow_rate",
+        "inlet_pressure"
+    ).write \
+        .format("jdbc") \
+        .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
+        .option("dbtable", "machine_events_bronze") \
+        .option("user", "admin") \
+        .option("password", REDSHIFT_PASSWORD) \
+        .option("driver", "com.amazon.redshift.jdbc.Driver") \
+        .mode("append") \
+        .save()
     
-
-# bronze_redshift_query = bronze_df.writeStream \
-#     .foreachBatch(write_machine_bronze_to_redshift) \
-#     .trigger(processingTime="2 seconds") \
-#     .option("checkpointLocation", "s3a://iot-platform-malek/checkpoints/machine_bronze_redshift") \
-#     .start()
+bronze_redshift_query = bronze_df.writeStream \
+    .foreachBatch(write_machine_bronze_to_redshift) \
+    .trigger(processingTime="2 seconds") \
+    .option("checkpointLocation", "s3a://iot-platform-malek/checkpoints/machine_bronze_redshift") \
+    .start()
 
 
 
@@ -643,7 +644,7 @@ bronze_s3_query = bronze_df.writeStream \
         
 #     ).write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option("dbtable", "machine_events_silver") \
 #         .option("user", "admin") \
 #         .option("password", REDSHIFT_PASSWORD) \
@@ -696,7 +697,7 @@ bronze_s3_query = bronze_df.writeStream \
 
 #     batch_df.write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option("dbtable", "machine_aggregates_gold") \
 #         .option("user", "admin") \
 #         .option("password", REDSHIFT_PASSWORD) \
@@ -1099,7 +1100,7 @@ worker_gold_df = (
 #         "validation_reason"
 #     ).write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option(
 #             "dbtable",
 #             "worker_events_quarantine"
@@ -1145,7 +1146,7 @@ worker_gold_df = (
 #         "fatigue_score"
 #     ).write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option(
 #             "dbtable",
 #             "worker_events_bronze"
@@ -1201,7 +1202,7 @@ worker_gold_df = (
 #         "alert_level"
 #     ).write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option(
 #             "dbtable",
 #             "worker_events_silver"
@@ -1243,7 +1244,7 @@ worker_gold_df = (
 
 #     batch_df.write \
 #         .format("jdbc") \
-#         .option("url", "jdbc:redshift://iot-platform-workgroup.533267199028.us-east-1.redshift-serverless.amazonaws.com:5439/dev") \
+#         .option("url", f"jdbc:redshift://{REDSHIFT_HOST}:{REDSHIFT_PORT}/{REDSHIFT_DB}") \
 #         .option(
 #             "dbtable",
 #             "worker_safety_gold"
